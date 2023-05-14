aeef2251bd7146073bdf6b01880c6cb58cc08278
Tarif007 <mth471994@gmail.com>
2023-05-14 17:29:12 -0400

    mm

--- service/routes.py
+++ service/routes.py
@@ -67,6 +67,7 @@ def create_accounts():
     # LIST ALL ACCOUNTS
     ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
         """
@@ -87,6 +88,7 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -110,6 +112,8 @@ def get_accounts(account_id):
     ######################################################################
     # UPDATE AN EXISTING ACCOUNT
     ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
         """
@@ -137,6 +141,8 @@ def update_accounts(account_id):
     ######################################################################
     # DELETE AN ACCOUNT
     ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
         """
