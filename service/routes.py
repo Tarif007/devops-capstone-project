f50cdc53df3952f105c60656ac1a4024bed67741
Tarif007 <mth471994@gmail.com>
2023-05-21 06:28:11 -0400

    fix spacing

--- service/routes.py
+++ service/routes.py
@@ -66,6 +66,7 @@ def create_accounts():
     ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
         """
@@ -78,7 +79,7 @@ def list_accounts():
         account_list = [account.serialize() for account in accounts]
 
         app.logger.info("Returning [%s] accounts", len(account_list))
-        return jsonify(account_list), status.HTTP_200_OK
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
@@ -97,7 +98,7 @@ def get_accounts(account_id):
         account = Account.find(account_id)
         if not account:
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        return account.serialize(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
@@ -124,7 +125,7 @@ def update_accounts(account_id):
         account.deserialize(request.get_json())
         account.update()
 
-        return account.serialize(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
@@ -148,7 +149,7 @@ def delete_accounts(account_id):
         if account:
             account.delete()
 
-        return "", status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
